c849b5b441a064fb9853aac8cb1c96200b37a13a
苏苏 <zkx2245717455@163.com>
2022-05-27 08:41:16 +0800

    Add files via upload

---
+++ 欢迎使用幕布｜入门指南.mm
@@ -0,0 +1,36 @@
+
+<map>
+  <node ID="root" TEXT="欢迎使用幕布｜入门指南">
+    <node TEXT="幕布是什么？" ID="32117230e5141600d" _mubu_text="%3Cspan%20class=%22bold%22%3E%E5%B9%95%E5%B8%83%E6%98%AF%E4%BB%80%E4%B9%88%EF%BC%9F%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="✍️ 简洁、易用的大纲笔记" ID="11017230ec34200b9" _mubu_text="%3Cspan%3E%E2%9C%8D%EF%B8%8F%20%E7%AE%80%E6%B4%81%E3%80%81%E6%98%93%E7%94%A8%E7%9A%84%E5%A4%A7%E7%BA%B2%E7%AC%94%E8%AE%B0%3C/span%3E" STYLE="fork"/>
+      <node TEXT="🎉 可以一键转换思维导图" ID="23d17230e536ae022" _mubu_text="%3Cspan%3E%F0%9F%8E%89%20%E5%8F%AF%E4%BB%A5%E4%B8%80%E9%94%AE%E8%BD%AC%E6%8D%A2%E6%80%9D%E7%BB%B4%E5%AF%BC%E5%9B%BE%3C/span%3E" STYLE="fork">
+        <node TEXT="幕布帮你" ID="1f617230eca4280d2" _mubu_text="%3Cspan%3E%E5%B9%95%E5%B8%83%E5%B8%AE%E4%BD%A0%3C/span%3E" STYLE="fork">
+          <node TEXT="“ 想得更清楚 ”" ID="3717230e536ae1811" _mubu_text="%3Cspan%3E%E2%80%9C%20%E6%83%B3%E5%BE%97%E6%9B%B4%E6%B8%85%E6%A5%9A%20%E2%80%9D%3C/span%3E" STYLE="fork"/>
+          <node TEXT="“ 理解更透彻 ”" ID="33c17230e536ae092" _mubu_text="%3Cspan%3E%E2%80%9C%20%E7%90%86%E8%A7%A3%E6%9B%B4%E9%80%8F%E5%BD%BB%20%E2%80%9D%3C/span%3E" STYLE="fork"/>
+        </node>
+        <node TEXT="像在你的大脑里，放了一个 “框架”" ID="1c917230e536ae0664" _mubu_text="%3Cspan%3E%E5%83%8F%E5%9C%A8%E4%BD%A0%E7%9A%84%E5%A4%A7%E8%84%91%E9%87%8C%EF%BC%8C%E6%94%BE%E4%BA%86%E4%B8%80%E4%B8%AA%20%E2%80%9C%3C/span%3E%3Cspan%20class=%22bold%22%3E%E6%A1%86%E6%9E%B6%3C/span%3E%3Cspan%3E%E2%80%9D%3C/span%3E" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="幕布能做什么？" ID="e917230eba87c169" _mubu_text="%3Cspan%20class=%22bold%22%3E%E5%B9%95%E5%B8%83%E8%83%BD%E5%81%9A%E4%BB%80%E4%B9%88%EF%BC%9F%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="📖 记笔记-课堂笔记、会议记录···" ID="33017230ebb3e1164" _mubu_text="%3Cspan%3E%F0%9F%93%96%20%E8%AE%B0%E7%AC%94%E8%AE%B0-%E8%AF%BE%E5%A0%82%E7%AC%94%E8%AE%B0%E3%80%81%E4%BC%9A%E8%AE%AE%E8%AE%B0%E5%BD%95%C2%B7%C2%B7%C2%B7%3C/span%3E" STYLE="fork"/>
+      <node TEXT="🌲 理结构-整理思路、拆解知识···" ID="1a317230ebbb9f096" _mubu_text="%3Cspan%3E%F0%9F%8C%B2%20%E7%90%86%E7%BB%93%E6%9E%84-%E6%95%B4%E7%90%86%E6%80%9D%E8%B7%AF%E3%80%81%E6%8B%86%E8%A7%A3%E7%9F%A5%E8%AF%86%C2%B7%C2%B7%C2%B7%3C/span%3E" STYLE="fork"/>
+      <node TEXT="✔️ 做待办-日程安排、购物清单···" ID="14a17230ebc15b146" _mubu_text="%3Cspan%3E%E2%9C%94%EF%B8%8F%20%E5%81%9A%E5%BE%85%E5%8A%9E-%E6%97%A5%E7%A8%8B%E5%AE%89%E6%8E%92%E3%80%81%E8%B4%AD%E7%89%A9%E6%B8%85%E5%8D%95%C2%B7%C2%B7%C2%B7%3C/span%3E" STYLE="fork"/>
+      <node TEXT="···" ID="3f328d069520417bc96418cd70babc27" _mubu_text="%3Cspan%3E%C2%B7%C2%B7%C2%B7%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="新手入门，只需4步" ID="7217230e36ef50a6" _mubu_text="%3Cspan%20class=%22bold%22%3E%E6%96%B0%E6%89%8B%E5%85%A5%E9%97%A8%EF%BC%8C%E5%8F%AA%E9%9C%804%E6%AD%A5%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="移动端" ID="3a81723123a35914b" _mubu_text="%3Cspan%20class=%22bold%22%3E%E7%A7%BB%E5%8A%A8%E7%AB%AF%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="Windows/Mac客户端&amp;网页版" ID="1be1723123e3de0c5" _mubu_text="%3Cspan%20class=%22bold%22%3EWindows/Mac%E5%AE%A2%E6%88%B7%E7%AB%AF&amp;amp;%E7%BD%91%E9%A1%B5%E7%89%88%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="1️⃣ 新建一篇幕布文档" ID="717231251c9b18b" _mubu_text="%3Cspan%20class=%22%20text-color-red%22%3E1%EF%B8%8F%E2%83%A3%20%3C/span%3E%3Cspan%20class=%22bold%20underline%20text-color-red%22%3E%E6%96%B0%E5%BB%BA%E4%B8%80%E7%AF%87%E5%B9%95%E5%B8%83%E6%96%87%E6%A1%A3%3C/span%3E" STYLE="fork">
+        <node TEXT="点击左上角「+」按钮" ID="8c659f516e4c560a2fe828aa04bdfeef" _mubu_text="%3Cspan%3E%E7%82%B9%E5%87%BB%E5%B7%A6%E4%B8%8A%E8%A7%92%3C/span%3E%3Cspan%20class=%22bold%22%3E%E3%80%8C+%E3%80%8D%3C/span%3E%3Cspan%3E%E6%8C%89%E9%92%AE%3C/span%3E" STYLE="fork"/>
+        <node TEXT="在下拉菜单选择「新建文档」" ID="f6ead713484ab3a146713e95bbcf2012" _mubu_text="%3Cspan%3E%E5%9C%A8%E4%B8%8B%E6%8B%89%E8%8F%9C%E5%8D%95%E9%80%89%E6%8B%A9%3C/span%3E%3Cspan%20class=%22bold%22%3E%E3%80%8C%E6%96%B0%E5%BB%BA%E6%96%87%E6%A1%A3%E3%80%8D%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="2️⃣ 创建主题，输入内容" ID="426e596bb4a7efff232c05736e80e02d" _mubu_text="%3Cspan%20class=%22%20text-color-red%22%3E2%EF%B8%8F%E2%83%A3%20%3C/span%3E%3Cspan%20class=%22bold%20underline%20text-color-red%22%3E%E5%88%9B%E5%BB%BA%E4%B8%BB%E9%A2%98%EF%BC%8C%E8%BE%93%E5%85%A5%E5%86%85%E5%AE%B9%3C/span%3E" STYLE="fork"/>
+      <node TEXT="3️⃣ 调整层级" ID="15c17231255a7f11d" _mubu_text="%3Cspan%20class=%22%20text-color-red%22%3E3%EF%B8%8F%E2%83%A3%20%3C/span%3E%3Cspan%20class=%22bold%20underline%20text-color-red%22%3E%E8%B0%83%E6%95%B4%E5%B1%82%E7%BA%A7%3C/span%3E" STYLE="fork"/>
+      <node TEXT="😍 妙哉！一键转换思维导图" ID="7b9eee5bdac59965e785564c515d9014" _mubu_text="%3Cspan%20class=%22%20text-color-red%22%3E%F0%9F%98%8D%20%3C/span%3E%3Cspan%20class=%22bold%20text-color-red%22%3E%E5%A6%99%E5%93%89%EF%BC%81%E4%B8%80%E9%94%AE%E8%BD%AC%E6%8D%A2%E6%80%9D%E7%BB%B4%E5%AF%BC%E5%9B%BE%3C/span%3E" STYLE="fork"/>
+      <node TEXT="更多使用技巧" ID="02a515f875e7b61290439fddfafb26c8" _mubu_text="%3Cspan%3E%E6%9B%B4%E5%A4%9A%E4%BD%BF%E7%94%A8%E6%8A%80%E5%B7%A7%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="更多使用技巧/问题解答：" ID="31b1723103a7ba14e" _mubu_text="%3Cspan%3E%E6%9B%B4%E5%A4%9A%E4%BD%BF%E7%94%A8%E6%8A%80%E5%B7%A7/%E9%97%AE%E9%A2%98%E8%A7%A3%E7%AD%94%EF%BC%9A%3C/span%3E" STYLE="bubble" POSITION="default"/>
+    <node TEXT="🎉 现在开始幕布的畅快笔记之旅吧！" ID="37617230ffb1a9098" _mubu_text="%3Cspan%20class=%22bold%22%3E%F0%9F%8E%89%20%E7%8E%B0%E5%9C%A8%E5%BC%80%E5%A7%8B%E5%B9%95%E5%B8%83%E7%9A%84%E7%95%85%E5%BF%AB%E7%AC%94%E8%AE%B0%E4%B9%8B%E6%97%85%E5%90%A7%EF%BC%81%3C/span%3E" STYLE="bubble" POSITION="default"/>
+  </node>
+</map>
